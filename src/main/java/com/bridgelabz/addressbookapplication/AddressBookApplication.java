db41fdac2922a1f688058da9cd33c6bfb0d1cdd4
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-06 03:33:37 +0530

    Modified: To switch log between custom environments

--- src/main/java/com/bridgelabz/addressbookapplication/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapplication/AddressBookApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbookapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,8 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
-		log.info("Address book Application started");
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address Book Application started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
