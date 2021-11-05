9adf0499a41c632889777df64eaa515706679fa8
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-06 03:58:14 +0530

    Added: Validations for fields in contact dto

--- src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
@@ -1,13 +1,25 @@
 package com.bridgelabz.addressbookapplication.dto;
 
+import javax.validation.constraints.Pattern;
+
 public class ContactDTO {
 
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "First name Invalid")
     public String firstName;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "Last name Invalid")
     public String lastName;
+
+    @Pattern(regexp = "(?!^\\d+$)^[A-Z,a-z,0-9, ()#-]{3,}$", message = "Address Invalid")
     public String address;
+
     public String state;
     public String city;
+
+    @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$", message = "Zip Code Invalid")
     public String zip;
+
+    @Pattern(regexp = "^([\\+]?\\d{2})?[\\s]?\\d{10}$", message = "Phone Invalid")
     public String phoneNumber;
 
     public ContactDTO(String firstName, String lastName, String address, String state, String city, String zip,
