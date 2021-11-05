2dfdeaf73ff35a569aa79d5d7401881760cad7b5
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-06 04:19:36 +0530

    Added: To store exception messages thrown

---
+++ src/main/java/com/bridgelabz/addressbookapplication/exceptions/AddressBookException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbookapplication.exceptions;
+
+public class AddressBookException extends RuntimeException {
+
+    public AddressBookException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/bridgelabz/addressbookapplication/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/bridgelabz/addressbookapplication/exceptions/AddressBookExceptionHandler.java
@@ -25,4 +25,11 @@ public class AddressBookExceptionHandler {
         ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errorMessage);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(AddressBookException.class)
+    public ResponseEntity<ResponseDTO> handleEmployeePayrollException(AddressBookException exception) {
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+
+    }
 }
