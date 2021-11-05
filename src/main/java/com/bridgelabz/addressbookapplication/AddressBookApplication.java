244932bcfefbc6784fe02d8a119835ae6a6b82f4
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-06 03:09:32 +0530

    Added: To log the application process

--- src/main/java/com/bridgelabz/addressbookapplication/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapplication/AddressBookApplication.java
@@ -3,11 +3,16 @@ package com.bridgelabz.addressbookapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address book Application started");
 	}
 
 }
