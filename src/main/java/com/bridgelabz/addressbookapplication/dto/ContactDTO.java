8a7979fe1745c982ba3f9a5916d4e684d9a8aa96
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-09 02:41:19 +0530

    Added: Validations for all properties

--- src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapplication.dto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import lombok.ToString;
@@ -7,21 +8,29 @@ import lombok.ToString;
 public @ToString class ContactDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "First name Invalid")
+    @NotBlank(message = "First name cannot be Empty")
     public String firstName;
 
     @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "Last name Invalid")
+    @NotBlank(message = "Last name cannot be Empty")
     public String lastName;
 
     @Pattern(regexp = "(?!^\\d+$)^[A-Z,a-z,0-9, ()#-]{3,}$", message = "Address Invalid")
+    @NotBlank(message = "Address cannot be Empty")
     public String address;
 
+    @NotBlank(message = "State cannot be Empty")
     public String state;
+
+    @NotBlank(message = "City cannot be Empty")
     public String city;
 
     @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$", message = "Zip Code Invalid")
+    @NotBlank(message = "Zip code cannot be Empty")
     public String zip;
 
     @Pattern(regexp = "^([\\+]?\\d{2})?[\\s]?\\d{10}$", message = "Phone Invalid")
+    @NotBlank(message = "Phone Number cannot be Empty")
     public String phoneNumber;
 
 }
