36de7c972cf4ef75912757983daf1334bb69e958
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-09 03:18:43 +0530

    Modified: To perform CRUD operations on repository directly

--- src/main/java/com/bridgelabz/addressbookapplication/services/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapplication/services/AddressBookService.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.addressbookapplication.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.bridgelabz.addressbookapplication.dto.ContactDTO;
@@ -17,44 +16,35 @@ public class AddressBookService implements IAddressBookService {
     @Autowired
     private AddressBookRepository addressBookRepository;
 
-    List<Contact> contactList = new ArrayList<>();
-
     @Override
     public List<Contact> getContact() {
 
-        return contactList;
+        return addressBookRepository.findAll();
     }
 
     @Override
     public Contact getContactById(int contactId) {
-        return contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
-                .orElseThrow(() -> new AddressBookException("Contact not found"));
+        return addressBookRepository.findById(contactId)
+                .orElseThrow(() -> new AddressBookException("Contact with id " + contactId + " does not exist..!"));
     }
 
     @Override
     public Contact createContact(ContactDTO contactDTO) {
-        Contact contact = new Contact(contactList.size() + 1, contactDTO);
-        contactList.add(contact);
+        Contact contact = new Contact(contactDTO);
         return addressBookRepository.save(contact);
     }
 
     @Override
     public Contact updateContact(int contactId, ContactDTO contactDTO) {
         Contact contact = this.getContactById(contactId);
-        contact.setFirstName(contactDTO.firstName);
-        contact.setLastName(contactDTO.lastName);
-        contact.setAddress(contactDTO.address);
-        contact.setState(contactDTO.state);
-        contact.setCity(contactDTO.city);
-        contact.setZip(contactDTO.zip);
-        contact.setPhoneNumber(contactDTO.phoneNumber);
-        contactList.set(contactId - 1, contact);
-        return contact;
+        contact.updateContact(contactDTO);
+        return addressBookRepository.save(contact);
     }
 
     @Override
     public void deleteContact(int contactId) {
-        contactList.remove(contactId - 1);
+        Contact contact = this.getContactById(contactId);
+        addressBookRepository.delete(contact);
     }
 
 }
