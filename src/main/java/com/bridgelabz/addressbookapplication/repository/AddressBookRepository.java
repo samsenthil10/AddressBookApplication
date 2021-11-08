6321e03840f7f90404d19931eb5086f9dc76534b
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-09 03:45:59 +0530

    Added: Custom query to get contacts by city or state

--- src/main/java/com/bridgelabz/addressbookapplication/repository/AddressBookRepository.java
+++ src/main/java/com/bridgelabz/addressbookapplication/repository/AddressBookRepository.java
@@ -1,9 +1,18 @@
 package com.bridgelabz.addressbookapplication.repository;
 
+import java.util.List;
+
 import com.bridgelabz.addressbookapplication.models.Contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AddressBookRepository extends JpaRepository<Contact, Integer> {
+
+    @Query(value = "select * from address_book where state=:state", nativeQuery = true)
+    List<Contact> findContactByState(String state);
+
+    @Query(value = "select * from address_book where city=:city", nativeQuery = true)
+    List<Contact> findContactByCity(String city);
 
 }
